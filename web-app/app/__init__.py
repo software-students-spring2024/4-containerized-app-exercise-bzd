23a25f9dda358498d40119df32e89e83c74e3d38
jinqiaocheng163 <joc297@nyu.edu>
2024-04-14 16:23:47 -0400

    pylint changes

--- web-app/app/__init__.py
+++ web-app/app/__init__.py
@@ -4,12 +4,16 @@ import os
 from flask import Flask
 from pymongo import MongoClient
 
-# Initialize the Flask application
-app = Flask(__name__)
+def create_app():
+    """factory function."""
+    app = Flask(__name__)
 
-# MongoDB connection setup
-mongo_uri = os.getenv('MONGO_URI', 'mongodb://localhost:27017/')
-client = MongoClient(mongo_uri)
-db = client["mongodb"]  # Replace with your actual database name
+    # MongoDB connection setup
+    mongo_uri = os.getenv('MONGO_URI', 'mongodb://localhost:27017/')
+    client = MongoClient(mongo_uri)
+    db = client["mongodb"]  # Replace with your actual database name
 
-from app import routes # pylint: disable=wrong-import-position
+    from app import routes
+    routes.init_app(app, db)
+
+    return app
--- web-app/app/routes.py
+++ web-app/app/routes.py
@@ -1,9 +1,11 @@
 """Define routes for the Flask application."""
 from flask import render_template
-from app import app, db
 
-@app.route('/')
-def index():
-    """Render index page with results from the database."""
-    results = db['precision'].find()  
-    return render_template('index.html', results=list(results))
+def init_app(app, db):
+    """pass app and db into routes."""
+    @app.route('/')
+    def index():
+        """Render index page with results from the database."""
+        results = db['precision'].find()
+        return render_template('index.html', results=list(results))
+ 

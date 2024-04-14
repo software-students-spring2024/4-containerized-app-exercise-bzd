44a4c2eed8abd8b4242e010909e1a1f0892e6865
YY35n <yinyiwen3589@gmail.com>
2024-04-14 13:57:30 -0400

    database

--- web-app/app/__init__.py
+++ web-app/app/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # MongoDB connection setup
 mongo_uri = os.environ.get('DATABASE_URL', 'mongodb://localhost:27017/')
 client = MongoClient(mongo_uri)
-db = client['your_database_name']  # Replace with your actual database name
+db = client["mongodb"]  # Replace with your actual database name
 
 # Import the routes after the Flask app is created
 from app import routes

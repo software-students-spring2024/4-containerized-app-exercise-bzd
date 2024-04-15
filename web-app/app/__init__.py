3c718318a12bb284a256f0848e1f3ef9819c7ea7
YY35n <yinyiwen3589@gmail.com>
2024-04-15 13:55:54 -0400

    change to app

--- web-app/app/__init__.py
+++ web-app/app/__init__.py
@@ -10,6 +10,18 @@ mongo_uri = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
 client = MongoClient(mongo_uri)
 db = client["image_classification"]  # Ensure you specify your actual database name
 
+
+def insert_sample_document():
+    """Insert a sample document into the MongoDB collection."""
+    image = "web-app/ad4c4c52-b21a-41d6-ba9a-cd79b0dc6db4.jpg"
+    
+    sample_document = {
+        "prediction": "cat",
+        "image": image,  # This should be binary data in a real scenario
+    }
+    db["prediction"].insert_one(sample_document)
+    print("Sample document inserted.")
+
 def init_app(app, db):
     """Pass app and db into routes."""
 
@@ -27,6 +39,7 @@ def init_app(app, db):
 
 # Initialize the app with routes
 init_app(app, db)
+insert_sample_document
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
